14e21d1f22749130106db61853809ab410624d84
Parthmodii <152470481+Parthmodii@users.noreply.github.com>
2024-02-01 23:04:57 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
+import Register from "./pages/register/Register";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+
+function App() {
+ return (
+<Router>
+      <Routes>
+      <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/" element={<Navigate replace to="/home" />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
